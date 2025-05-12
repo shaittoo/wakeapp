14602f75ce4bbee980ba2ec5d09b806b4c93dad0
Shaina Talisay <talisayshaina8@gmail.com>
2025-05-12 15:08:02 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/wakeapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wakeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screen/map_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(10.6395288, 122.2215462), // UPV Miagao coordinates
+    zoom: 16.0, // Adjust zoom as you like
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8,
+    target: LatLng(10.6395288, 122.2215462), // UPV Miagao coordinates
+    tilt: 59.44,
+    zoom: 19.0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
