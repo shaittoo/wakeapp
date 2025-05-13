be94bbf77b0d6354e7bacc5889d47bc6bee64b58
Shaina Talisay <talisayshaina8@gmail.com>
2025-05-13 20:34:54 +0800

    HomePage [Shaina]

---
+++ lib/screen/timer_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatelessWidget {
+  const TimerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Timer Page', style: TextStyle(fontSize: 24))),
+    );
+  }
+} 
