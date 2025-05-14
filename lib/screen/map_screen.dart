df4b4a16b420f9e9f38718df7b95409798fc950b
Shaina Talisay <talisayshaina8@gmail.com>
2025-05-15 01:59:05 +0800

    env file added

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -5,8 +5,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
 import 'package:location/location.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const kGoogleApiKey = 'AIzaSyCv3FFr20CIXT48UA5LdiO_eEffceacY0Q';
+final kGoogleApiKey = dotenv.env['GOOGLE_MAPS_API_KEY']!;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -37,6 +38,7 @@ class _MapScreenState extends State<MapScreen> {
     super.initState();
     searchController.addListener(_onChange);
     _getUserLocation();
+    debugPrint(kGoogleApiKey);
   }
 
   Future<void> _getUserLocation() async {
